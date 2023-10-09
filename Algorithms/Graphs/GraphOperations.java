1299d5f10d0192c8f1ab80ff2a7f72c67567a850
Eason <ejin458@aucklanduni.ac.nz>
2023-10-09 16:51:18 +1300

    Iterative BFS on non-weighted graph

--- Algorithms/Graphs/GraphOperations.java
+++ Algorithms/Graphs/GraphOperations.java
@@ -2,9 +2,17 @@ package Graphs;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class GraphOperations {
+  /**
+   * Performs a depth first search on a graph and returns the order of the vertices visited
+   *
+   * @param graph
+   * @return
+   */
   public static ArrayList<Integer> DfsList(HashMap<Integer, ArrayList<Integer>> graph) {
     ArrayList<Integer> visited = new ArrayList<Integer>();
     Stack<Integer> s = new Stack<Integer>();
@@ -16,6 +24,30 @@ public class GraphOperations {
           visited.add(current);
           for (int i = graph.get(current).size() - 1; i >= 0; i--) {
             s.push(graph.get(current).get(i));
+          }
+        }
+      }
+    }
+    return visited;
+  }
+
+  /**
+   * Performs a breadth first search on a graph and returns the order of the vertices visited
+   *
+   * @param graph
+   * @return
+   */
+  public static ArrayList<Integer> BfsList(HashMap<Integer, ArrayList<Integer>> graph) {
+    ArrayList<Integer> visited = new ArrayList<Integer>();
+    Queue<Integer> q = new LinkedList<Integer>();
+    if (!graph.isEmpty()) {
+      q.add(0);
+      while (!q.isEmpty()) {
+        int current = q.poll();
+        if (!visited.contains(current)) {
+          visited.add(current);
+          for (int i = 0; i < graph.get(current).size(); i++) {
+            q.add(graph.get(current).get(i));
           }
         }
       }
