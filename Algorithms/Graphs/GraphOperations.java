7110aa6ba721330419badaa4f7958a4d9ce91b5b
Eason <ejin458@aucklanduni.ac.nz>
2023-10-09 17:14:16 +1300

    Added null checker

--- Algorithms/Graphs/GraphOperations.java
+++ Algorithms/Graphs/GraphOperations.java
@@ -14,17 +14,18 @@ public class GraphOperations {
    * @return
    */
   public static ArrayList<Integer> DfsList(HashMap<Integer, ArrayList<Integer>> graph, int start) {
+    if (graph == null || graph.isEmpty()) {
+      return null;
+    }
     ArrayList<Integer> visited = new ArrayList<Integer>();
     Stack<Integer> s = new Stack<Integer>();
-    if (!graph.isEmpty()) {
-      s.push(start);
-      while (!s.isEmpty()) {
-        int current = s.pop();
-        if (!visited.contains(current)) {
-          visited.add(current);
-          for (int i = graph.get(current).size() - 1; i >= 0; i--) {
-            s.push(graph.get(current).get(i));
-          }
+    s.push(start);
+    while (!s.isEmpty()) {
+      int current = s.pop();
+      if (!visited.contains(current)) {
+        visited.add(current);
+        for (int child : graph.get(current)) {
+          s.push(child);
         }
       }
     }
@@ -38,24 +39,50 @@ public class GraphOperations {
    * @return
    */
   public static ArrayList<Integer> BfsList(HashMap<Integer, ArrayList<Integer>> graph, int start) {
+    if (graph == null || graph.isEmpty()) {
+      return null;
+    }
     ArrayList<Integer> visited = new ArrayList<Integer>();
     Queue<Integer> q = new LinkedList<Integer>();
-    if (!graph.isEmpty()) {
-      q.add(start);
-      while (!q.isEmpty()) {
-        int current = q.poll();
-        if (!visited.contains(current)) {
-          visited.add(current);
-          for (int i = 0; i < graph.get(current).size(); i++) {
-            q.add(graph.get(current).get(i));
-          }
+    q.add(start);
+    while (!q.isEmpty()) {
+      int current = q.poll();
+      if (!visited.contains(current)) {
+        visited.add(current);
+        for (int child : graph.get(current)) {
+          q.add(child);
         }
       }
     }
     return visited;
   }
 
-  // public static int findGirthList(HashMap<Integer, ArrayList<Integer>> graph) {
-    
-  // }
+  public static int findGirthList(HashMap<Integer, ArrayList<Integer>> graph) {
+    if (graph == null || graph.isEmpty()) {
+      return 0;
+    }
+    int[] dist = new int[graph.size()];
+    // Perform BFS from each vertex
+    for (int v = 0; v < graph.size(); v++) {
+      for (int a = 0; a < graph.size(); a++) {
+        dist[a] = -1;
+      }
+      Queue<Integer> q = new LinkedList<Integer>();
+      dist[v] = 0;
+      q.add(v);
+      while (!q.isEmpty()) {
+        int current = q.poll();
+        for (int child : graph.get(current)) {
+          if (dist[child] == -1) {
+            dist[child] = dist[current] + 1;
+            q.add(child);
+          } else if (child != current && dist[child] >= 0) {
+            // Found a cycle, return its depth
+            return dist[current] + dist[child] + 1;
+          }
+        }
+      }
+    }
+    return 0;
+  }
 }
